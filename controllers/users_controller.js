936492212e0d92cf3d28682a5c60be226a25a791
gauravlodhi <gauravlodhi983@gmail.com>
2023-06-26 15:47:26 +0530

    layouts setting

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@ module.exports.users = function(req, res) {
     return res.end('<h1>Users</h1>')
 }
 module.exports.profile = function(req, res) {
-    return res.end('<h1>Profile_BY_Gaurav</h1>')
+    return res.render('users_profile', { title: 'Profile' });
 }
 module.exports.login = function(req, res) {
     return res.end('<h1>Login by me</h1>');
